ab6e8b2faf04f89a9330d43598b2cf64de928e58
Front <27463495+Frontesque@users.noreply.github.com>
2022-01-24 18:16:53 -0500

    move to NUXT

--- NUXT/layouts/default.vue
+++ NUXT/layouts/default.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app dark>
+
+    <v-card v-model="value" class="topNav rounded-0">
+      <span>NotShitTube</span>
+    </v-card>
+
+
     <v-app-content>
       <nuxt />
     </v-app-content>
 
-
-    <v-bottom-navigation v-model="value" >
+    <v-bottom-navigation v-model="value" class="bottomNav rounded-0">
       <v-btn v-for="(item, i) in tabs" :key="i">
         <span v-text="item.name" />
 
@@ -16,6 +21,16 @@
 
   </v-app>
 </template>
+
+<style scoped>
+.topNav {
+  padding: 1em;
+}
+.bottomNav {
+  position: absolute;
+  bottom: 0;
+}
+</style>
 
 <script>
   export default {
