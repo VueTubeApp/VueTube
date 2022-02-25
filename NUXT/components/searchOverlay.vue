c05239f5959ed5681159bae65ad5f44a8cc424ec
Kenny <27463495+Frontesque@users.noreply.github.com>
2022-02-25 13:54:15 -0500

    add $youtube.autoComplete()

--- NUXT/components/searchOverlay.vue
+++ NUXT/components/searchOverlay.vue
@@ -3,6 +3,29 @@
     <v-text-field
       label="Search"
       outlined
+      v-model="text"
+      @input="textChanged"
     />
+    {{ response }}
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: null,
+      response: null,
+    }
+  },
+
+  methods: {
+    textChanged() {
+      this.$youtube.autoComplete(this.text, (data) => {
+        this.response = data;
+      });
+    }
+  }
+
+}
+</script>
--- NUXT/plugins/youtube.js
+++ NUXT/plugins/youtube.js
@@ -1,6 +1,14 @@
+import { Http } from '@capacitor-community/http';
+
 const module = {
-  test() {
-    return "success";
+  async autoComplete(text, callback) {
+
+    const options = {
+      url: 'https://suggestqueries-clients6.youtube.com/complete/search',
+      params: { client: 'youtube', q: text },
+    };
+    const response = await Http.request(options);
+
   }
 }
 
