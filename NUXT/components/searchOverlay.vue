ff3d92b8381565f5857b842368785f2005d8ae40
Front <27463495+Frontesque@users.noreply.github.com>
2022-02-26 15:15:30 -0500

    parse search to make it readable

--- NUXT/components/searchOverlay.vue
+++ NUXT/components/searchOverlay.vue
@@ -6,7 +6,11 @@
       v-model="text"
       @input="textChanged"
     />
-    {{ response }}
+    <v-list style="min-width: 180px;">
+      <v-list-item v-for="(item, index) in response" :key="index">
+        <v-btn text style="width: 100%;" class="info--text">{{ item }}</v-btn>
+      </v-list-item>
+    </v-list>
   </div>
 </template>
 
@@ -15,14 +19,15 @@ export default {
   data() {
     return {
       text: null,
-      response: null,
+      response: [],
     }
   },
 
   methods: {
     textChanged() {
       this.$youtube.autoComplete(this.text, (res) => {
-        this.response = res.data;
+        const data = res.data.replace(/^.*?\(/,'').replace(/\)$/,''); //Format Response
+        this.response = JSON.parse(data)[1]
       });
     }
   }
