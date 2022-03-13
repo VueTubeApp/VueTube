fa876c02a640e18e0c4ecbe4854f9516906a69b3
Front <27463495+Frontesque@users.noreply.github.com>
2022-03-13 16:39:24 -0400

    fix issue #12

--- NUXT/layouts/default.vue
+++ NUXT/layouts/default.vue
@@ -58,6 +58,13 @@
 html, body {
   overflow-x: hidden;
 }
+
+p, span, div {
+  -webkit-user-select: none; /* Safari */        
+  -moz-user-select: none; /* Firefox */
+  -ms-user-select: none; /* IE10+/Edge */
+  user-select: none; /* Standard */
+}
 </style>
 
 <style scoped>
