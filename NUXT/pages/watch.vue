87789763da3751953a007c2170fc74bf2f5ac8e8
Sushi <57518856+SushiPython@users.noreply.github.com>
2022-03-18 19:24:01 -0700

    slightly more working watch page
    this should work when innertube api is fixed

--- NUXT/pages/watch.vue
+++ NUXT/pages/watch.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
     <video controls autoplay :src="vidSrc" width="100%" height="300vh" />
-    {{ vidSrc }}
+    <v-card class="ml-2 mr-2 flat light">
+      <h2>{{ title }}</h2>
+      <span>{{ views }} views • Published {{ uploaded }}</span>
+      {{ likes }} Likes / {{ dislikes }} Dislikes
+      <p>{{ description }}</p>
+      </h3>
+    </v-card>
   </div>
 </template>
 
@@ -9,12 +15,35 @@
 export default {
   data() {
     return {
-      vidSrc: ""
+      likes: this.likes,
+      uploaded: this.uploaded,
+      dislikes: this.dislikes,
+      vidSrc: this.vidSrc,
+      description: this.description,
+      views: this.views
     }
   },
   mounted() {
-    const { v } = this.$route.query
-    console.log(v, this.$youtube.getVid(v))
+      
+    this.$youtube.getVid(this.$route.query.v).then(result => {
+      console.log('Video info data')
+      console.log(result);
+      this.vidSrc = result.streamingData.formats[0].url
+      this.title = result.videoDetails.title
+      this.description = result.videoDetails.shortDescription;
+      this.views = result.videoDetails.viewCount;
+    });
+
+    
+    this.$youtube.getRemainingVideoInfo(this.$route.query.v, (data) => {
+      this.likes = data.likes,
+      this.uploaded = data.uploadDate;
+    });
+    
+    this.$youtube.getReturnYoutubeDislike(this.$route.query.v, (data) => {
+      this.dislikes = data.dislikes;
+    });
+
   }
 }
 </script>
