29e999336ec165cc37863e57c216a6fcef18eb9b
Nikita Krupin <krupin.nikita0110@gmail.com>
2022-03-23 19:58:35 -0400

    treeShake test

--- NUXT/nuxt.config.js
+++ NUXT/nuxt.config.js
@@ -49,6 +49,7 @@ export default {
 
   vuetify: {
     customVariables: ["~/assets/variables.scss"],
+    treeShake: true,
     theme: {
       dark: false,
       options: { customProperties: true },
