d00421ff9af3ac517eef8dedf83db62dc4c9c361
Front <27463495+Frontesque@users.noreply.github.com>
2022-04-04 18:25:12 -0400

    style: :sparkles: add dependancies

--- NUXT/plugins/tempPlugins/demoPlugin.js
+++ NUXT/plugins/tempPlugins/demoPlugin.js
@@ -1,11 +1,30 @@
 module.exports = {
   manifest: {
-    name: "Test plugin",
+    name: "Test plugin", // Required
     vuetube: "*",
-    version: "1.0",
-    author: "Frontesque",
-    description: "A plugin to test how vuetube handles plugins // Add 'Hello, World!' to the home page.",
-    homepage: "https://vuetube.app",
-    license: "GPL-3.0"
+    version: "1.0", // Required
+    author: "Frontesque", // Required
+    description: "A plugin to test how vuetube handles plugins // Add 'Hello, World!' to the home page.", // Required
+    homepage: "https://github.com/Frontesque/VueTube-Example-Plugin",
+    license: "GPL-3.0" // Required
+  },
+
+  /************************* 
+   * Execute code on ALL VueTube pages
+   ************************/
+  global: function() {
+
+  },
+
+  /************************* 
+   * Execute code on SPECIFIC VueTube pages
+   * 
+   * You can view what page you intend to inject code into by looking at `~/NUXT/pages/` in the main VueTube repo
+   ************************/
+  pages: {
+    home: function() { // Execute code on JUST the home page
+
+    }
   }
+
 }
