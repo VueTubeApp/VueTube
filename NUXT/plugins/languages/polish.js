28d0376c1fad4ca780c1bfee590198049e677ed4
Dominik <dominikponiszewski@gmail.com>
2022-06-15 22:40:06 +1200

    lang: added polish language pack

---
+++ NUXT/plugins/languages/polish.js
@@ -0,0 +1,53 @@
+module.exports = {
+  name: "Polish",
+
+  global: {
+    home: "Główna",
+    subscriptions: "Subskrypcje",
+    library: "Biblioteka",
+  },
+
+  index: {
+    connecting: "Łączenie",
+    launching: "Uruchamianie",
+  },
+
+  settings: {
+    general: "Ogólne",
+    theme: "Motyw",
+    player: "Odtwarzacz",
+    uitweaker: "Interfejs Użytkownika",
+    startupoptions: "Opcje Uruchamiania",
+    plugins: "Rozszerzenia",
+    updates: "Aktualizacje",
+    logs: "Logi",
+    about: "Informacje",
+    devmode: "Edytor Rejestru",
+  },
+
+  mods: {
+    startup: {
+      defaultpage: "Strona Domyślna",
+    },
+    about: {
+      appinformation: "Informacje O Aplikacji",
+      appversion: "Wersja Aplikacji",
+      deviceinformation: "Informacje O Urządzeniu",
+      platform: "Platforma",
+      os: "System Operacyjny",
+      model: "Model",
+      manufacturer: "Manufaktura",
+      emulator: "Emulator",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "Witaj w VueTube",
+    tagline: "Przyszłość Streamingu Wideo",
+    next: "DALEJ",
+    updated: "VueTube został zaktualizowany!",
+    awesome: "Świetnie!",
+  },
+};
