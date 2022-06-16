7697570f5b6b242259a896a82ce30809098ddd68
Johan le stickman <Johan_Stickman@proton.me>
2022-06-16 18:07:21 +0200

    Create french.js

---
+++ NUXT/plugins/languages/french.js
@@ -0,0 +1,84 @@
+module.exports = {
+  name: "Français",
+
+  global: {
+    home: "Accueil",
+    subscriptions: "Abonnements",
+    library: "Bibliothèque"
+  },
+
+  index: {
+    connecting: "Connexion",
+    launching: "Lancement"
+  },
+
+  settings: {
+    general: "Général",
+    theme: "Thème",
+    player: "Lecteur",
+    uitweaker: "Interface",
+    startupoptions: "Options de démarrage",
+    plugins: "Plugins",
+    updates: "Mises à jour",
+    logs: "Logs",
+    about: "À propos",
+    devmode: "Éditeur de registre"
+  },
+
+  mods: {
+    general: {
+      language: "Langage"
+    },
+    theme: {
+      normal: "Normal",
+      adaptive: "Adaptatif",
+      custom: "Personnalisé",
+      dark: "Sombre",
+      black: "Noir",
+      darkmode: "Thème sombre",
+      darkmodetagline: "Bravo Six, Going Dark."
+    },
+    tweaks: {
+      fullscreen: "Plein écran",
+      navbarblur: "Barre de navigation flou",
+      roundedcorners: "Coin arrondi",
+      roundthumbnails: "Miniatures arrondies",
+      roundwatchpagecomponents: "Page d'accueil arrondis",
+      radius: "Rayon",
+    },
+    startup: {
+      defaultpage: "Page par défaut"
+    },
+    updates: {
+      install: "Installer",
+      view: "Voir",
+      latest: "Plus récente",
+      installed: "Installée",
+    },
+    logs: {
+      more: "Afficher plus",
+    },
+    about: {
+      appinformation: "Informations sur l'application",
+      appversion: "Version de l'application",
+      deviceinformation: "Informations sur l'appareil",
+      platform: "Platforme",
+      os: "Système d'exploitation",
+      model: "Modèle",
+      manufacturer: "Fabricant",
+      emulator: "Emulateur",
+      github: "Github",
+      discord: "Discord"
+    }
+  },
+
+  events: {
+    welcome: "Bienvenue sur VueTube!",
+    tagline: "Le futur du streaming vidéo.",
+    next: "Suivant",
+    updated: "VueTube a été mis à jour !",
+    awesome: "Incroyable !"
+  }
+
+
+}
