54be500efe118e7e8f7afdeb472751cd56644d0f
Nizami <104249296+Nizami20052022@users.noreply.github.com>
2022-06-23 10:30:27 +0400

    azerbaijan.js
    I have added an Azerbaijani translation and I am sure that my translations are correct.

---
+++ NUXT/plugins/languages/azerbaijan.js
@@ -0,0 +1,93 @@
+module.exports = {
+  name: "Azerbaijan",
+
+  global: {
+    home: "Ev",
+    subscriptions: "Abunəliklər",
+    library: "Kitabxana",
+    restart: "Yenidən başladın",
+    later: "Daha sonra",
+    settingRestart: "Bu seçimin dəyişdirilməsi, dəyişikliklərin tətbiq edilməsi üçün tətbiqin yenidən başladılmasını tələb edir."
+  }, 
+
+  index: {
+    connecting: "Qoşulur",
+    plugins: "Pluginlər yüklənir",
+    launching: "Başladılır",
+  },
+
+  settings: {
+    general: "Ümumi",
+    theme: "Mövzu",
+    player: "Oynadıcı",
+    uitweaker: "UI Çimdikləmə",
+    startupoptions: "Başlatma Seçimləri",
+    plugins: "Pluginlər",
+    updates: "Yeniləmələr",
+    logs: "Jurnallar",
+    about: "Haqqında",
+    devmode: "Reyestr redaktoru",
+  },
+
+  mods: {
+    general: {
+      language: "Dil",
+    },
+    theme: {
+      normal: "Normal",
+      adaptive: "Adaptiv",
+      custom: "Fərdi",
+      dark: "Qaranlıq",
+      black: "Qara",
+      darkmode: "Qaranlıq rejim",
+      darkmodetagline: "Bravo, Qaranlıq işlənir." ,
+    },
+    tweaks: {
+      fullscreen: "Tam ekran",
+      navbarblur: "Naviqasiya Paneli bulanması",
+      roundedcorners: "Dairəvi Künclər",
+      roundthumbnails: "Dəyirmi Eskizlər",
+      roundwatchpagecomponents: "Dəyirmi Baxma Səhifə Komponentləri",
+      radius: "Radius",
+    },
+    startup: {
+      defaultpage: "Defolt Səhifə",
+    },
+    updates: {
+      install: "Quraşdırın",
+      view: "Baxın",
+      latest: "Ən son",
+      installed: "Quraşdırılıb",
+    },
+    logs: {
+      more: "Daha çox",
+    },
+    about: {
+      appinformation: "Tətbiq Məlumatı",
+      appversion: "Tətbiq Versiyası",
+      deviceinformation: "Cihaz Məlumatı",
+      platform: "Platforma",
+      os: "Əməliyyat Sistemi",
+      model: "Model",
+      manufacturer: "İstehsalçı",
+      emulator: "Emulator",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "VueTube-a Xoş Gəlmisiniz",
+    tagline: "Video yayımının gələcəyi",
+    next: "Növbəti",
+    updated: "VueTube Yeniləndi!",
+    awesome: "Möhtəşəm!",
+    langsetup: "Gəlin Dil Seçək!",
+    featuresetup: "Bəzi Xüsusiyyətləri Seçək!",
+    enableryd: "Return YouTube Dislike'ı Aktivləşdirin",
+    enablespb: "SponsorBlok'u Aktivləşdirin",
+    thanks: "VueTube istifadə etdiyiniz üçün təşəkkür edirik",
+    enjoy: "Ümid edirik ki, gözəl təcrübəniz olacaq",
+    packageinstaller: "Endirmək Üçün Paket Seçin"
+  },
+};
