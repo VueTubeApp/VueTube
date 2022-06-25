6201b4dabe8a68b89be6977f87e4ffc0d1b8661d
Rick Slierendregt <71077562+Mcfly2340@users.noreply.github.com>
2022-06-25 14:14:40 +0200

    Added packageinstaller line to dutch.js

--- NUXT/plugins/languages/dutch.js
+++ NUXT/plugins/languages/dutch.js
@@ -1,95 +1,93 @@
 module.exports = {
-    name: "Nederlands",
-  
-    global: {
-      home: "Home",
-      subscriptions: "Abonnementen",
-      library: "Bibliotheek",
-      restart: "Start nu opnieuw op",
-      later: "Later",
-      settingRestart: "Het wijzigen van deze instelling vereist dat de app opnieuw wordt opgestart om de instellingen toe te passen."
+  name: "Nederlands",
+
+  global: {
+    home: "Home",
+    subscriptions: "Abonnementen",
+    library: "Bibliotheek",
+    restart: "Start nu opnieuw op",
+    later: "Later",
+    settingRestart: "Het wijzigen van deze instelling vereist dat de app opnieuw wordt opgestart om de instellingen toe te passen."
+  },
+
+  index: {
+    connecting: "Verbinden",
+    plugins: "Plugins Laden",
+    launching: "Opstarten"
+  },
+
+  settings: {
+    general: "Algemeen",
+    theme: "Thema",
+    player: "Videospeler",
+    uitweaker: "Gebruikersinterface",
+    startupoptions: "Opstartopties",
+    plugins: "Plugins",
+    updates: "Updates",
+    logs: "Logboeken",
+    about: "Over",
+    devmode: "Register bewerken"
+  },
+
+  mods: {
+    general: {
+      language: "Taal"
     },
-  
-    index: {
-      connecting: "Verbinden",
-      plugins: "Plugins Laden",
-      launching: "Opstarten"
+    theme: {
+      normal: "Normaal",
+      adaptive: "Adaptief",
+      custom: "Aangepast",
+      dark: "Donker",
+      black: "Zwart",
+      darkmode: "Donkere modus",
+      darkmodetagline: "Bravo Six, Going Dark."
     },
-  
-    settings: {
-      general: "Algemeen",
-      theme: "Thema",
-      player: "Videospeler",
-      uitweaker: "Gebruikersinterface",
-      startupoptions: "Opstartopties",
-      plugins: "Plugins",
-      updates: "Updates",
-      logs: "Logboeken",
-      about: "Over",
-      devmode: "Register bewerken"
+    tweaks: {
+      fullscreen: "Volledig Scherm",
+      navbarblur: "Vervaging Navigatiebalk",
+      roundedcorners: "Afgeronde Hoeken",
+      roundthumbnails: "Ronde Hoeken In Video Miniaturen",
+      roundwatchpagecomponents: "Ronde Hoeken In Video Speler Componenten",
+      radius: "Straal"
     },
-  
-    mods: {
-      general: {
-        language: "Taal"
-      },
-      theme: {
-        normal: "Normaal",
-        adaptive: "Adaptief",
-        custom: "Aangepast",
-        dark: "Donker",
-        black: "Zwart",
-        darkmode: "Donkere modus",
-        darkmodetagline: "Bravo Six, Going Dark."
-      },
-      tweaks: {
-        fullscreen: "Volledig Scherm",
-        navbarblur: "Vervaging Navigatiebalk",
-        roundedcorners: "Afgeronde Hoeken",
-        roundthumbnails: "Ronde Hoeken In Video Miniaturen",
-        roundwatchpagecomponents: "Ronde Hoeken In Video Speler Componenten",
-        radius: "Straal"
-      },
-      startup: {
-        defaultpage: "Standaard Pagina"
-      },
-      updates: {
-        install: "Installeer",
-        view: "Bekijk",
-        latest: "Laatste",
-        installed: "Geïnstalleerd"
-      },
-      logs: {
-        more: "Meer"
-      },
-      about: {
-        appinformation: "Applicatieinformatie",
-        appversion: "Applicatieversie",
-        deviceinformation: "Apparaatinformatie",
-        platform: "Platform",
-        os: "Besturingssysteem",
-        model: "Model",
-        manufacturer: "Fabrikant",
-        emulator: "Emulator",
-        github: "GitHub",
-        discord: "Discord"
-      }
+    startup: {
+      defaultpage: "Standaard Pagina"
     },
-  
-    events: {
-      welcome: "Welkom bij VueTube",
-      tagline: "De toekomst van videostreaming",
-      next: "Volgende",
-      updated: "VueTube is bijgewerkt!",
-      awesome: "Geweldig!",
-      langsetup: "Laten we een taal kiezen!",
-      featuresetup: "Laten we wat functies kiezen!",
-      enableryd: "Return YouTube Dislike aanzetten",
-      enablespb: "SponsorBlock aanzetten",
-      thanks: "Bedankt dat je VueTube gebruikt",
-      enjoy: "We hopen dat je een geweldige ervaring hebt"
-    }
-  
-  
-  }
+    updates: {
+      install: "Installeer",
+      view: "Bekijk",
+      latest: "Laatste",
+      installed: "Geïnstalleerd"
+    },
+    logs: {
+      more: "Meer"
+    },
+    about: {
+      appinformation: "Applicatieinformatie",
+      appversion: "Applicatieversie",
+      deviceinformation: "Apparaatinformatie",
+      platform: "Platform",
+      os: "Besturingssysteem",
+      model: "Model",
+      manufacturer: "Fabrikant",
+      emulator: "Emulator",
+      github: "GitHub",
+      discord: "Discord"
+    },
+  },
+
+  events: {
+    welcome: "Welkom bij VueTube",
+    tagline: "De toekomst van videostreaming",
+    next: "Volgende",
+    updated: "VueTube is bijgewerkt!",
+    awesome: "Geweldig!",
+    langsetup: "Laten we een taal kiezen!",
+    featuresetup: "Laten we wat functies kiezen!",
+    enableryd: "Return YouTube Dislike aanzetten",
+    enablespb: "SponsorBlock aanzetten",
+    thanks: "Bedankt dat je VueTube gebruikt",
+    enjoy: "We hopen dat je een geweldige ervaring hebt",
+    packageinstaller: "Selecteer een bestand om te downloaden"
+  },
 };
