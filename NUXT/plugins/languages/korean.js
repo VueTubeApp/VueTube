ca789323d4199947d1a951ae5957629982bf8a99
Chico <jjjm6000@gmail.com>
2022-06-25 15:01:00 +0900

    add korean translation (#410)
    * add korean
    
    * add korean
    
    * after watched video
    
    Bravo Six, Going Dark - CALL OF DUTY: MODERN WARFARE
    
    * Update korean.js

---
+++ NUXT/plugins/languages/korean.js
@@ -0,0 +1,93 @@
+module.exports = {
+  name: "한국어",
+
+  global: {
+    home: "홈",
+    subscriptions: "구독",
+    library: "라이브러리",
+    restart: "재시작",
+    later: "나중에",
+    settingRestart: "이 설정을 수정하려면 응용 프로그램을 다시 시작해야 변경 사항이 적용됩니다."
+  },
+
+  index: {
+    connecting: "연결 중",
+    plugins: "플러그인 로드 중",
+    launching: "시작 중",
+  },
+
+  settings: {
+    general: "일반",
+    theme: "테마",
+    player: "플레이어",
+    uitweaker: "UI 트윅",
+    startupoptions: "시작 옵션",
+    plugins: "플러그인",
+    updates: "업데이트",
+    logs: "로그",
+    about: "정보",
+    devmode: "레지스트리 편집기",
+  },
+
+  mods: {
+    general: {
+      language: "언어",
+    },
+    theme: {
+      normal: "기본",
+      adaptive: "적응형",
+      custom: "사용자 정의",
+      dark: "다크",
+      black: "블랙",
+      darkmode: "다크 모드",
+      darkmodetagline: "브라보 식스... 어둠속으로.",
+    },
+    tweaks: {
+      fullscreen: "전체 화면",
+      navbarblur: "탐색 표시줄 흐리게",
+      roundedcorners: "둥근 모서리",
+      roundthumbnails: "둥근 썸네일",
+      roundwatchpagecomponents: "둥근 영상 하단 구성 요소",
+      radius: "반경",
+    },
+    startup: {
+      defaultpage: "시작 화면",
+    },
+    updates: {
+      install: "설치",
+      view: "보기",
+      latest: "최신",
+      installed: "설치됨",
+    },
+    logs: {
+      more: "더보기",
+    },
+    about: {
+      appinformation: "앱 정보",
+      appversion: "앱 버전",
+      deviceinformation: "장치 정보",
+      platform: "플랫폼",
+      os: "운영 체제",
+      model: "모델",
+      manufacturer: "제조사",
+      emulator: "에뮬레이터",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "VueTube에 오신 것을 환영합니다",
+    tagline: "비디오 스트리밍의 미래",
+    next: "다음",
+    updated: "VueTube가 업데이트되었어요!",
+    awesome: "대박!",
+    langsetup: "언어를 선택해주세요!",
+    featuresetup: "이제 추가 기능을 선택해주세요!",
+    enableryd: "Return YouTube Dislike 활성화",
+    enablespb: "SponsorBlock 활성화",
+    thanks: "VueTube를 사용해 주셔서 감사합니다",
+    enjoy: "즐거운 경험 되시길 바라겠습니다",
+    packageinstaller: "다운로드할 패키지 선택"
+  },
+};
