54d867d30ea44a5e070a7bd4228c8023462539c1
Danik2343 <38865696+Danik2343@users.noreply.github.com>
2022-06-26 19:39:21 +0300

    Add Russian Translation
    Add Russian Translation

---
+++ NUXT/plugins/languages/russian.js
@@ -0,0 +1,93 @@
+module.exports = {
+  name: "Русский",
+
+  global: {
+    home: "Главная",
+    subscriptions: "Подписки",
+    library: "Библиотека",
+    restart: "Перезагрузить",
+    later: "Позже",
+    settingRestart: "Изменение этих параметров требует перезапуска приложения, чтобы изменения вступили в силу.",
+  },
+
+  index: {
+    connecting: "Подключение",
+    plugins: "Загрузка плагинов",
+    launching: "Загрузка",
+  },
+
+  settings: {
+    general: "Общие",
+    theme: "Тема",
+    player: "Плеер",
+    uitweaker: "Модификация интерфейса",
+    startupoptions: "Параметры запуска",
+    plugins: "Плагины",
+    updates: "Обновления",
+    logs: "Логи",
+    about: "О программе",
+    devmode: "Редактор реестра",
+  },
+
+  mods: {
+    general: {
+      language: "Язык",
+    },
+    theme: {
+      normal: "Обычная",
+      adaptive: "Адаптивная",
+      custom: "Пользоватская",
+      dark: "Тёмная",
+      black: "Чёрная",
+      darkmode: "Тёмный режим",
+      darkmodetagline: "Браво Six, Наступает Тьма.",
+    },
+    tweaks: {
+      fullscreen: "Полный экран",
+      navbarblur: "Размытие навигационной панели",
+      roundedcorners: "Закругленные углы",
+      roundthumbnails: "Круглые миниатюры",
+      roundwatchpagecomponents: "Круглые компоненты страницы просмотра",
+      radius: "Радиус",
+    },
+    startup: {
+      defaultpage: "Страница по умолчанию",
+    },
+    updates: {
+      install: "Установить",
+      view: "Просмотр",
+      latest: "Последнее",
+      installed: "Установлено",
+    },
+    logs: {
+      more: "Подробнее",
+    },
+    about: {
+      appinformation: "Информация о приложении",
+      appversion: "Версия приложения",
+      deviceinformation: "Информация об устройстве",
+      platform: "Платформа",
+      os: "Операционная система",
+      model: "Модель",
+      manufacturer: "Произовдитель",
+      emulator: "Эмулятор",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "Добро пожаловать в VueTube",
+    tagline: "Это будущее видео потока",
+    next: "Далее",
+    updated: "VueTube был обновлен!",
+    awesome: "Отлично!",
+    langsetup: "Давайте выберем язык!",
+    featuresetup: "Давайте выберем некоторые функции!",
+    enableryd: "Выключить Return YouTube Dislike",
+    enablespb: "Выключить SponsorBlock",
+    thanks: "Спасибо за использование VueTube",
+    enjoy: "Мы надеемся, что у вас будет удивительный опыт",
+    packageinstaller: "Выберите пакет для загрузки",
+  },
+};
