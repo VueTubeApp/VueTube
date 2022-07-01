96be64ce946eaeed36ca72f2ba6f72e3eced5c72
huutrunglovepc <106517171+huutrunglovepc@users.noreply.github.com>
2022-07-02 06:35:57 +0700

    Add Vietnamese Translations (#433)

---
+++ NUXT/plugins/languages/vietnamese.js
@@ -0,0 +1,93 @@
+module.exports = {
+  name: "Tiếng Việt",
+
+  global: {
+    home: "Home",
+    subscriptions: "Đăng ký",
+    library: "Thư viện",
+    restart: "Khởi động lại",
+    later: "Để sau",
+    settingRestart: "Thay đổi cài đặt này yêu cầu khởi động lại để áp dụng các thay đổi."
+  },
+
+  index: {
+    connecting: "Đang kết nối",
+    plugins: "Đang tải plugins",
+    launching: "Đang khởi chạy",
+  },
+
+  settings: {
+    general: "Chung",
+    theme: "Chủ đề",
+    player: "Player",
+    uitweaker: "Tùy chỉnh UI",
+    startupoptions: "Tùy chọn khởi động",
+    plugins: "Plugins",
+    updates: "Cập nhật",
+    logs: "Nhật kí",
+    about: "Về",
+    devmode: "Chỉnh sửa Registry",
+  },
+
+  mods: {
+    general: {
+      language: "Ngôn ngữ",
+    },
+    theme: {
+      normal: "Bình thường",
+      adaptive: "Tương thích",
+      custom: "Tùy chỉnh",
+      dark: "Tối",
+      black: "Đen",
+      darkmode: "Chế độ tối",
+      darkmodetagline: "Tắt đèn thôi",
+    },
+    tweaks: {
+      fullscreen: "Toàn màn hình",
+      navbarblur: "Làm mờ thanh điều hướng",
+      roundedcorners: "Bo góc",
+      roundthumbnails: "Bo góc hình thu nhỏ",
+      roundwatchpagecomponents: "Bo góc các mục trang xem video",
+      radius: "Mức độ bo",
+    },
+    startup: {
+      defaultpage: "Trang mặc định",
+    },
+    updates: {
+      install: "Cài đặt",
+      view: "Xem",
+      latest: "Mới nhất",
+      installed: "Đã cài đặt",
+    },
+    logs: {
+      more: "Nhiều hơn",
+    },
+    about: {
+      appinformation: "Thông tin ứng dụng",
+      appversion: "Phiên bản ứng dụng",
+      deviceinformation: "Thông tin thiết bị",
+      platform: "Nền tảng",
+      os: "Hệ điều hành",
+      model: "Mã",
+      manufacturer: "Nhà sản xuất",
+      emulator: "Máy ảo",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "Chào mừng to VueTube",
+    tagline: "Tương lai của video streaming",
+    next: "Tiếp",
+    updated: "VueTube đã được cập nhật!",
+    awesome: "Tuyệt vời!",
+    langsetup: "Chọn một ngôn ngữ!",
+    featuresetup: "Chọn một số tính năng!",
+    enableryd: "Bật Return YouTube Dislike",
+    enablespb: "Bật SponsorBlock",
+    thanks: "Cảm on đã sủ dụng VueTube",
+    enjoy: "Chúng mình hi vọng bạn sẽ có một trải nghiệm tuyệt vời",
+    packageinstaller: "Chọn một phiên bản để tải về"
+  },
+};
