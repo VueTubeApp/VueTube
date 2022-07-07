b77ca1755820dde1e1c971c903aec9680ba1b869
Fjuro <fjuro@seznam.cz>
2022-07-07 11:11:57 +0200

    Create czech.js

---
+++ NUXT/plugins/languages/czech.js
@@ -0,0 +1,93 @@
+module.exports = {
+  name: "Čeština",
+
+  global: {
+    home: "Domů",
+    subscriptions: "Odběry",
+    library: "Knihovna",
+    restart: "Restartovat",
+    later: "Později",
+    settingRestart: "Úprava tohoto nastavení vyžaduje restart aplikace pro provedení změn."
+  },
+
+  index: {
+    connecting: "Připojování",
+    plugins: "Načítání doplňků",
+    launching: "Spouštění",
+  },
+
+  settings: {
+    general: "Obecné",
+    theme: "Téma",
+    player: "Přehrávač",
+    uitweaker: "Rozhraní",
+    startupoptions: "Možnosti spuštění",
+    plugins: "Doplňky",
+    updates: "Aktualizace",
+    logs: "Protokol",
+    about: "O aplikaci",
+    devmode: "Editor registrů",
+  },
+
+  mods: {
+    general: {
+      language: "Jatyk",
+    },
+    theme: {
+      normal: "Normální",
+      adaptive: "Adaptivní",
+      custom: "Vlastní",
+      dark: "Tmavé",
+      black: "Černé",
+      darkmode: "Tmavý režim",
+      darkmodetagline: "Bravo Six, Going Dark.",
+    },
+    tweaks: {
+      fullscreen: "Celá obrazovka",
+      navbarblur: "Rozmazat navigační lištu",
+      roundedcorners: "Zakulacené rohy",
+      roundthumbnails: "Zakulacené náhledy",
+      roundwatchpagecomponents: "Zakulacené komponenty stránky sledování",
+      radius: "Poloměr",
+    },
+    startup: {
+      defaultpage: "Výchozí stránka",
+    },
+    updates: {
+      install: "Instalovat",
+      view: "Zobrazit",
+      latest: "Nejnovější",
+      installed: "Nainstalováno",
+    },
+    logs: {
+      more: "Více",
+    },
+    about: {
+      appinformation: "Informace o aplikace",
+      appversion: "Verze aplikace",
+      deviceinformation: "Informace o zařízení",
+      platform: "Platforma",
+      os: "Operační systém",
+      model: "Model",
+      manufacturer: "Výrobce",
+      emulator: "Emulátor",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "Vítejte ve VueTube",
+    tagline: "Budoucnosti streamování videa",
+    next: "Další",
+    updated: "VueTube byl aktualizován!",
+    awesome: "Úžasné!",
+    langsetup: "Pojďme si vybrat jazyk!",
+    featuresetup: "Vyberte si pár funkcí",
+    enableryd: "Povolit Return YouTube Dislike",
+    enablespb: "Povolit SponsorBlock",
+    thanks: "Děkujeme za používání VueTube",
+    enjoy: "Doufáme, že se vám aplikace bude líbit",
+    packageinstaller: "Vyberte balíček ke stažení"
+  },
+};
