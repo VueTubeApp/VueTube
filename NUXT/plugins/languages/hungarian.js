011546b7c2dbab11928dcf25a362fdfbe2198728
ferivoq <36544651+ferivoq@users.noreply.github.com>
2022-08-10 13:17:48 +0200

    Translating VueTube to Hungarian
    Finish of hungarian.js

---
+++ NUXT/plugins/languages/hungarian.js
@@ -0,0 +1,107 @@
+module.exports = {
+  name: "Hungarian",
+
+  global: {
+    home: "Kezdőlap",
+    subscriptions: "Feliratkozások",
+    library: "Könyvtár",
+    restart: "Újraindítás",
+    later: "Később",
+    settingRestart: "Ennek a beállításnak az igénybevételéhez újra kell indítani az applikációt.",
+    okay: "Oké"
+  },
+
+  index: {
+    connecting: "Csatlakozás",
+    plugins: "Pluginok betöltése",
+    launching: "Elindítás",
+  },
+
+  settings: {
+    general: "General",
+    theme: "Téma",
+    player: "Lejátszó",
+    uitweaker: "UI Tweaker",
+    startupoptions: "Elindítási beállítások",
+    plugins: "Pluginok",
+    updates: "Frissítések",
+    logs: "Naplózás",
+    about: "Rólunk",
+    devmode: "Fejlesztői mód",
+  },
+
+  mods: {
+    general: {
+      language: "Nyelv",
+    },
+    theme: {
+      normal: "Normal",
+      adaptive: "Alkalmazkodó",
+      custom: "Egyéni",
+      dark: "Sötét",
+      black: "Fekete",
+      darkmode: "Sötét mód",
+      darkmodetagline: "Bravo Six, Going Dark.",
+    },
+    tweaks: {
+      fullscreen: "Teljes képernyő",
+      navbarblur: "Navbar Elhomályosítása",
+      roundedcorners: "Lekerekített Sarkok",
+      roundthumbnails: "Lekerekített Thumbnailek",
+      roundwatchpagecomponents: "Lekerekített videó gombok",
+      radius: "Rádiusz",
+    },
+    startup: {
+      defaultpage: "Alapértelmezett oldal",
+    },
+    updates: {
+      updating: "Frissítés letöltése",
+      checking: "Frissítések keresése",
+      available: "Frissítés elérhető",
+      noupdate: "Nincs elérhető frissítés",
+      noupdatemessage: "A legújabb verzióját használod a VueTube-nak. Nézz vissza később a frissítésekért.",
+
+      installed: "Telepített Verzió",
+      latest: "Legújabb Verzió",
+
+      published: "Közzétett",
+      users: "Felhasználók",
+      size: "Frissítési Méret",
+
+      okay: "Oké",
+      refresh: "Újra töltés",
+      update: "Frissítés",
+      later: "Később",
+    },
+    logs: {
+      more: "Több",
+    },
+    about: {
+      appinformation: "App Információja",
+      appversion: "App Verziója",
+      deviceinformation: "Eszköz Információ",
+      platform: "Platform",
+      os: "Operációs Rendszer",
+      model: "Model",
+      manufacturer: "Gyártó",
+      emulator: "Emulátor",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "Üdvözöljük a VueTube-ban",
+    tagline: "A videó streaming jövője",
+    next: "Következő",
+    updated: "A VueTube frissítve lett",
+    awesome: "Csodás",
+    langsetup: "Válasszunk ki egy nyelvet!",
+    featuresetup: "Válasszunk ki néhány funkciót!",
+    enableryd: "Return YouTube Dislike Engedélyezzése",
+    enablespb: "SponsorBlock Engedélyezzése",
+    thanks: "Köszönjük, hogy a VueTube-ot használod",
+    enjoy: "Reméljük, csodálatos élményben lesz része",
+    packageinstaller: "Válassza ki a letölteni kívánt csomagot"
+  },
+};
