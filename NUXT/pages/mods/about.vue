edb3909f5784df5ebf22d9c008f22ffc3c323089
Nikita Krupin <krupin.nikita0110@gmail.com>
2022-08-20 18:23:34 -0400

    chore: lint mods/about.vue

--- NUXT/pages/mods/about.vue
+++ NUXT/pages/mods/about.vue
@@ -22,10 +22,13 @@
       "
       :style="{ borderRadius: `${roundTweak / 2}rem` }"
     >
-      <v-card-title><v-icon style="margin-right: 0.5em;">mdi-cog-box</v-icon>{{ lang.appinformation }}</v-card-title>
+      <v-card-title>
+        <v-icon style="margin-right: 0.5em">mdi-cog-box</v-icon>
+        {{ lang.appinformation }}
+      </v-card-title>
       <v-card-text>
         <h3>{{ lang.appversion }}</h3>
-        {{ version.substring(0, 7) || "Unknown" }}  ({{ release }})
+        {{ version.substring(0, 7) || "Unknown" }} ({{ release }})
       </v-card-text>
     </v-card>
     <!--   End App Information   -->
@@ -39,7 +42,10 @@
       "
       :style="{ borderRadius: `${roundTweak / 2}rem` }"
     >
-      <v-card-title><v-icon style="margin-right: 0.5em;">mdi-cellphone-information</v-icon>{{ lang.deviceinformation }}</v-card-title>
+      <v-card-title
+        ><v-icon style="margin-right: 0.5em">mdi-cellphone-information</v-icon
+        >{{ lang.deviceinformation }}</v-card-title
+      >
       <v-card-text>
         <h3>{{ lang.platform }}</h3>
         {{ deviceInfo.platform || "Unknown" }}<br />
