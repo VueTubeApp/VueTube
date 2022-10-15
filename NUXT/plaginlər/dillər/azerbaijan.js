049cb0788d586ae12c7cee66f99898b2acbd40e0
Maqa <104249296+Nizami20052022@users.noreply.github.com>
2022-10-15 23:26:03 +0400

    Update azerbaijan.js
    The Azerbaijani language has been updated!❤️

---
+++ NUXT/plaginlər/dillər/azerbaijan.js
@@ -0,0 +1,145 @@
+module.exports = {
+  name: "Azerbaijan",
+
+  global: {
+    home: "Ev",
+    subscriptions: "Abunəliklər",
+    library: "Kitabxana",
+    restart: "Yenidən başladın",
+    later: "Daha sonra",
+    settingRestart:
+      "Bu seçimin dəyişdirilməsi dəyişikliklərin tətbiq edilməsi üçün tətbiqin yenidən başladılmasını tələb edir.",
+    okay: "Oldu",
+  },
+
+  pages: {
+    index: "",
+    home: "Ev",
+    library: "Kitabxana",
+    search: "Axtarın",
+    settings: "Tənzimləmələr",
+    subscriptions: "Abunəliklər",
+    watch: "İzlə",
+    about: "Haqqında",
+    developer: "İnkişaf Etdirici",
+    general: "Ümumi",
+    player: "Oynadıcı",
+    plugins: "Əlavələr",
+    startup: "Başlat",
+    theme: "Mövzu",
+    tweaks: "tənzimləmələr",
+    updates: "Yeniləmələr"
+  },
+
+  index: {
+    connecting: "Qoşulur",
+    plugins: "Əlavələr yüklənir",
+    launching: "Başladılır",
+  },
+
+  settings: {
+    general: "Ümumi",
+    theme: "Mövzu",
+    player: "Oynadıcı",
+    uitweaker: "UI Tənzimləyici",
+    startupoptions: "Başlatma Seçimləri",
+    plugins: "Əlavələr",
+    updates: "Yeniləmələr",
+    logs: "Jurnallar",
+    about: "Haqqında",
+    devmode: "Qeyd redaktoru",
+  },
+
+  mods: {
+    general: {
+      language: "Dil",
+      backup: "Backup",
+      backupinfo: "Backup or restore your application settings",
+      restore: "Restore"
+    },
+    theme: {
+      normal: "Normal",
+      adaptive: "Adaptiv",
+      custom: "Fərdi",
+      dark: "Qaranlıq",
+      black: "Qara",
+      darkmode: "Qaranlıq rejim",
+      darkmodetagline: "Bravo, Qaralma gedir.",
+    },
+    tweaks: {
+      fullscreen: "Tam ekran",
+      navbarblur: "Naviqasiya Paneli bulanması",
+      roundedcorners: "Dairəvi Künclər",
+      roundthumbnails: "Dəyirmi Eskizlər",
+      roundwatchpagecomponents: "Dəyirmi Baxma Səhifə Komponentləri",
+      radius: "Radius",
+    },
+    startup: {
+      defaultpage: "Defolt Səhifə",
+    },
+    updates: {
+      updating: "Yeniləmə endirilir",
+      checking: "Yeniləmələr yoxlanılır",
+      available: "Yeniləmə mövcuddur",
+      noupdate: "Heç bir yeniləmə yoxdur",
+      noupdatemessage:
+        "VueTube-un ən son versiyasından istifadə edirsiniz. Yeniləmələr üçün daha sonra yoxlayın.",
+
+      installed: "Quraşdırılmış Versiya",
+      latest: "Son Versiya",
+
+      published: "Dərc edilmişdir",
+      users: "İstifadəçilər",
+      size: "Ölçüsünü yeniləyin",
+
+      okay: "Oldu",
+      refresh: "yenilə",
+      update: "Yeniləyin",
+      later: "Daha sonra",
+    },
+    logs: {
+      more: "Daha çox",
+    },
+    about: {
+      appinformation: "Tətbiq Məlumatı",
+      appversion: "Tətbiq Versiyası",
+      deviceinformation: "Cihaz Məlumatı",
+      platform: "Platforma",
+      os: "Əməliyyat sistemi",
+      model: "Model",
+      manufacturer: "İstehsalçı",
+      emulator: "Emulator",
+      github: "GitHub",
+      discord: "Discord",
+    },
+    developer: {
+      registryeditor: "Reyestr redaktoru",
+      registrywarning: "GİRİŞLƏRİN DƏYİŞİLMƏSİ TƏTBİQİNİZİN SINMASINA SƏBƏB OLA BİLƏR!",
+      createentry: "Giriş yaradın",
+      createentryfull: "Reyestr girişi yaradın",
+      cancel: "İmtina",
+      create: "Yaradın",
+      key: "Açar",
+      value: "Dəyər",
+      confirmdelete: "Silməni təsdiqləyin",
+      areyousure: "Silmək istədiyinizə əminsiniz",
+      delete: "Sil",
+      change: "Dəyiş",
+    },
+  },
+
+  events: {
+    welcome: "VueTube-a xoş gəlmisiniz",
+    tagline: "Video axınının gələcəyi",
+    next: "Növbəti",
+    updated: "VueTube yeniləndi",
+    awesome: "Möhtəşəm",
+    langsetup: "Gəlin Dil Seçək!",
+    featuresetup: "Bəzi Xüsusiyyətləri Seçək!",
+    enableryd: "Return YouTube Dislike'ı Aktivləşdir",
+    enablespb: "SponsorBlock'u aktivləşdir",
+    thanks: "VueTube istifadə etdiyiniz üçün təşəkkür edirik",
+    enjoy: "Ümid edirik ki, gözəl təcrübəniz olacaq",
+    packageinstaller: "Endirmək üçün paket seçin",
+  },
+};
