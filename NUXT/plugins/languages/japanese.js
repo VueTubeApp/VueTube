c1c40ec085e3574ccfccfc7e70ccef60022c6467
唐澤 克幸 <70247011+Katsuyuki-Karasawa@users.noreply.github.com>
2022-10-19 06:26:33 +0900

    add japanese.js and translate (#565)

---
+++ NUXT/plugins/languages/japanese.js
@@ -0,0 +1,146 @@
+module.exports = {
+    name: "日本語",
+  
+    global: {
+      home: "ホーム",
+      subscriptions: "登録チャンネル",
+      library: "ライブラリ",
+      restart: "Restart",
+      later: "Later",
+      settingRestart:
+        "変更を適用するためにアプリの再起動が必要です。",
+      okay: "OK",
+    },
+  
+    pages: {
+      index: "",
+      home: "ホーム",
+      library: "ライブラリ",
+      search: "検索",
+      settings: "設定",
+      subscriptions: "登録チャンネル",
+      watch: "視聴",
+      about: "About",
+      developer: "開発者",
+      general: "全般",
+      player: "プレーヤー",
+      plugins: "プラグイン",
+      startup: "起動時",
+      theme: "テーマ",
+      tweaks: "Tweaks",
+      updates: "アップデート"
+    },
+  
+    index: {
+      connecting: "Connecting",
+      plugins: "Loading Plugins",
+      launching: "Launching",
+    },
+  
+    settings: {
+      general: "全般",
+      theme: "テーマ",
+      player: "プレーヤー",
+      uitweaker: "UI Tweaker",
+      startupoptions: "起動時の設定",
+      plugins: "プラグイン",
+      updates: "アップデート",
+      logs: "ログ",
+      about: "About",
+      devmode: "開発者モード",
+    },
+  
+    mods: {
+      general: {
+        language: "言語",
+        backup: "バックアップ",
+        backupinfo: "アプリの設定をバックアップ、または復元する。",
+        restore: "復元"
+      },
+      theme: {
+        normal: "通常",
+        adaptive: "Adaptive",
+        custom: "カスタム",
+        dark: "ダーク",
+        black: "Black",
+        darkmode: "ダークモード",
+        darkmodetagline: "Bravo Six, Going Dark.(ブラボー6 通信を終了する)",
+      },
+      tweaks: {
+        fullscreen: "フルスクリーン",
+        navbarblur: "ナビゲーションバーをぼかす",
+        roundedcorners: "角丸",
+        roundthumbnails: "丸みを帯びたサムネイル",
+        roundwatchpagecomponents: "視聴画面のコンポーネントを丸く",
+        radius: "半径",
+      },
+      startup: {
+        defaultpage: "起動時のページ",
+      },
+      updates: {
+        updating: "アップデートをダウンロード中",
+        checking: "アップデートを確認",
+        available: "アップデートがあります!",
+        noupdate: "アップデートはありませんでした...",
+        noupdatemessage:
+          "最新バージョンを利用中。 後日更新を確認してください。",
+  
+        installed: "インストール済のバージョン",
+        latest: "最新バージョン",
+  
+        published: "Published",
+        users: "Users",
+        size: "Update Size",
+  
+        okay: "OK",
+        refresh: "Refresh",
+        update: "Update",
+        later: "Later",
+      },
+      logs: {
+        more: "詳細",
+      },
+      about: {
+        appinformation: "アプリの情報",
+        appversion: "アプリのバージョン",
+        deviceinformation: "デバイスの情報",
+        platform: "プラットフォーム",
+        os: "OS",
+        model: "モデル名",
+        manufacturer: "メーカー",
+        emulator: "エミュレータ",
+        github: "GitHub",
+        discord: "Discord",
+      },
+      developer: {
+        registryeditor: "レジストリエディタ",
+        registrywarning: "エントリをを変更するとアプリが破損する可能性があります!",
+        createentry: "エントリを作成",
+        createentryfull: "レジストリのエントリを作成",
+        cancel: "キャンセル",
+        create: "作成",
+        key: "Key",
+        value: "Value",
+        confirmdelete: "削除を確認",
+        areyousure: "本当に削除しますか?",
+        delete: "削除",
+        change: "変更",
+      },
+    },
+  
+    events: {
+      welcome: "ようこそVueTubeへ",
+      tagline: "The future of video streaming",
+      next: "次へ",
+      updated: "VueTubeはアップデートされました。",
+      awesome: "Awesome",
+      langsetup: "言語を選択",
+      featuresetup: "いくつかの機能を選択",
+      enableryd: "Return YouTube Dislikeを有効化",
+      enablespb: "SponsorBlockを有効化",
+      thanks: "VueTubeをご利用いただきありがとうございます。",
+      enjoy: "素晴らしい体験になるよう願っています",
+      packageinstaller: "Select a Package to Download",
+    },
+  };
+  
