082368ca0b5dbf124585c93d26aa2d5e736ba2dc
IDCL <dclprogramme@gmail.com>
2022-11-04 10:28:11 +0530

    created malayalam.js

---
+++ NUXT/plugins/languages/malayalam.js
@@ -0,0 +1,145 @@
+module.exports = {
+  name: "മലയാളം",
+
+  global: {
+    home: "ഇല്ലം",
+    subscriptions: "വരികൾ",
+    library: "ലൈബ്രറി",
+    restart: "വീണ്ടുും തുടങ്ങുക",
+    later: "പിന്നീട്",
+    settingRestart:
+      "ഈ ഏർപ്പാടുകൾ മാറ്റിയാൽ, അവ നിലവിൽ വരാനായി ആപ്പ് വീണ്ടുും തുടങ്ങേണ്ടതായി വരുും.",
+    okay: "ശരി",
+  },
+
+  pages: {
+    index: "ചൂണ്ടി",
+    home: "ഇല്ലം",
+    library: "ലൈബ്രറി",
+    search: "തിരയുക",
+    settings: "ഏർപ്പാടുകൾ",
+    subscriptions: "വരികൾ",
+    watch: "കാണുക",
+    about: "കുറിച്ച്",
+    developer: "ചമയ്ക്കുന്നയാൾ",
+    general: "പൊതു",
+    player: "വീഡിയോകാട്ടി",
+    plugins: "കൂട്ടിച്ചേർക്കലുകൾ",
+    startup: "തുടക്കം",
+    theme: "ചായം",
+    tweaks: "മാറ്റങ്ങൾ",
+    updates: "മെച്ചപ്പെടുത്തലുകൾ"
+  },
+
+  index: {
+    connecting: "കൂട്ടിയിണക്കുന്നു",
+    plugins: "കൂട്ടിച്ചേർക്കലുകൾ കയറ്റുന്നു",
+    launching: "തുറക്കുന്നു",
+  },
+
+  settings: {
+    general: "പൊതു",
+    theme: "ചായം",
+    player: "വീഡിയോകാട്ടി",
+    uitweaker: "ഇടപഴക്കമീടു മാറ്റി",
+    startupoptions: "തുടക്ക ഏർപ്പാടുകൾ",
+    plugins: "കൂട്ടിച്ചേർക്കലുകൾ",
+    updates: "മെച്ചപ്പെടുത്തലുകൾ",
+    logs: "പതിവുകൾ",
+    about: "കുറിച്ച്",
+    devmode: "ചമയ്ക്കൽ മട്ട്",
+  },
+
+  mods: {
+    general: {
+      language: "മൊഴി",
+      backup: "പിന്തുണകൊടുക്കുക",
+      backupinfo: "താങ്കളുടെ ആപ്പ് ഏർപ്പാടുകൾ വീണ്ടെടുക്കുക അല്ലെങ്കിൽ പിന്തുണകൊടുക്കുക",
+      restore: "വീണ്ടെടുക്കുക"
+    },
+    theme: {
+      normal: "സാധാരണ",
+      adaptive: "ഇഴുകുന്ന",
+      custom: "ഇഷ്ട",
+      dark: "ഇരുൾ",
+      black: "കറുപ്പ്",
+      darkmode: "ഇരുൾ മട്ട്",
+      darkmodetagline: "നിങ്ങൾ ഇരുളിലേക്ക് പോകുന്നു.",
+    },
+    tweaks: {
+      fullscreen: "മുഴുത്തിര",
+      navbarblur: "നാവ്ബാർ മങ്ങൽ",
+      roundedcorners: "വട്ടത്തിലുള്ള മൂലകൾ",
+      roundthumbnails: "വട്ടത്തിലുള്ള ചെറുപടം",
+      roundwatchpagecomponents: "വട്ടത്തിലുള്ള കാഴ്ചാതാൾ പങ്കുകൾ",
+      radius: "ആരം",
+    },
+    startup: {
+      defaultpage: "യഥാർത്ഥ താൾ",
+    },
+    updates: {
+      updating: "മെച്ചപ്പെട്ട പതിപ്പ് ഇറക്കിവയ്ക്കുന്നു",
+      checking: "മെച്ചപ്പെട്ട പതിപ്പിനായി തിരയുന്നു",
+      available: "മെച്ചപ്പെട്ട പതിപ്പ് ഉണ്ട്",
+      noupdate: "നിലവിൽ മെച്ചപ്പെട്ട പതിപ്പുകൾ ഇല്ല",
+      noupdatemessage:
+        "താങ്കൾ വ്യൂടൂബിന്റെ ഏറ്റവുും പുതിയ പതിപ്പാണ് ഇട്ടിരിക്കുന്നത്. മെച്ചപ്പെട്ട പതിപ്പുകൾക്കായി പിന്നീട് തിരയുക.",
+
+      installed: "ഇട്ടിരിക്കുന്ന പതിപ്പ്",
+      latest: "ഏറ്റവും പുതിയ പതിപ്പ്",
+
+      published: "പുറത്തുവിട്ടത്",
+      users: "ഉപയോഗിക്കുന്നവർ",
+      size: "പുതുക്കൽ വലിപ്പം",
+
+      okay: "ശരി",
+      refresh: "പുതുക്കുക",
+      update: "മെച്ചപ്പെടുത്തൽ",
+      later: "പിന്നീട്",
+    },
+    logs: {
+      more: "കുടുതൽ",
+    },
+    about: {
+      appinformation: "ആപ്പ് വിവരം",
+      appversion: "ആപ്പ് പതിപ്പ്",
+      deviceinformation: "ഡിവൈസ് വിവരം",
+      platform: "തിട്ട",
+      os: "ഓടൽ ചുറ്റുപാട്",
+      model: "മാതിരി",
+      manufacturer: "ഉണ്ടാക്കിയത്",
+      emulator: "എമുലേറ്റർ",
+      github: "ഗിറ്റ്ഹബ്",
+      discord: "ഡിസ്കോർഡ്",
+    },
+    developer: {
+      registryeditor: "Registry editor",
+      registrywarning: "CHANGING ENTRIES MAY CAUSE YOUR APP TO BREAK!",
+      createentry: "Create entry",
+      createentryfull: "Create registry entry",
+      cancel: "വേണ്ടെന്നു വയ്ക്കുക",
+      create: "ഉണ്ടാക്കുക",
+      key: "താഴ്കോൽ",
+      value: "വില",
+      confirmdelete: "നീക്കംചെയ്യൽ ഉറപ്പിക്കുക",
+      areyousure: "നീക്കംചെയ്യണം എന്ന് നിങ്ങൾക്ക് ഉറപ്പാണോ?",
+      delete: "നീക്കംചെയ്യുക",
+      change: "മാറ്റുക",
+    },
+  },
+
+  events: {
+    welcome: "വ്യൂടൂബിലേക്ക് വരവേൽപ്പ്",
+    tagline: "വീഡിയോ കാണിക്കലിന്റെ പുതിയ വരുംകാലം",
+    next: "അടുത്തത്",
+    updated: "വ്യൂടൂബ് മെച്ചപ്പെടുത്തി",
+    awesome: "കൊള്ളാം",
+    langsetup: "ഒരു മൊഴി തിരഞ്ഞെടുക്കാം!",
+    featuresetup: "കുറച്ചു തരങ്ങൾ തിരഞ്ഞെടുക്കാം!",
+    enableryd: "യൂട്ര്യൂബ് ഡിസ്ലൈക്കിലേക്ക് തിരികെ പോകുക",
+    enablespb: "സ്പോൺസർതടയൽ ചെയ്യുക",
+    thanks: "വ്യൂടൂബ് ഉപയോഗിച്ചതിൽ നന്നി",
+    enjoy: "നിങ്ങൾക്ക് ഒരു നല്ല ഇടപഴക്കകം ഉണ്ടാകും എന്ന് ഞങ്ങൾ കരുതുന്നു",
+    packageinstaller: "ഇറക്കിവയ്ക്കാനായി ഒരു പൊതിക്കെട്ട് തിരഞ്ഞെടുക്കുക",
+  },
+};
