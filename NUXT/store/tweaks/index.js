2ad2ed4fb6851c636830254c32c806eef434ed5f
Nikita Krupin <krupin.nikita0110@gmail.com>
2022-12-26 16:20:19 -0500

    Some improvement for launching screen closes #585

--- NUXT/store/tweaks/index.js
+++ NUXT/store/tweaks/index.js
@@ -2,16 +2,21 @@ export const state = () => ({
   roundTweak: 0,
   roundThumb: null,
   roundWatch: null,
+  launchTheme: null,
+  launchIconTheme: null,
 });
 export const mutations = {
   initTweaks(state) {
     if (process.client) {
-      state.roundTweak = JSON.parse(localStorage.getItem("roundTweak")) || 0;
+      state.roundTweak = JSON.parse(localStorage.getItem("roundTweak")) || 0; // defaults to 0
       state.roundThumb =
-        JSON.parse(localStorage.getItem("roundThumb")) === true;
+        JSON.parse(localStorage.getItem("roundThumb")) === true; // defaults to false
       state.roundWatch =
-        JSON.parse(localStorage.getItem("roundWatch")) === true;
+        JSON.parse(localStorage.getItem("roundWatch")) === true; // defaults to false
     }
+    state.launchTheme = JSON.parse(localStorage.getItem("launchTheme")) || 0; // defaults to 0
+    state.launchIconTheme =
+      JSON.parse(localStorage.getItem("launchIconTheme")) === true; // defaults to false
   },
   setRoundTweak(state, payload) {
     if (!isNaN(payload)) {
@@ -26,5 +31,13 @@ export const mutations = {
   setRoundWatch(state, payload) {
     state.roundWatch = payload;
     localStorage.setItem("roundWatch", payload);
+  },
+  setLaunchTheme(state, payload) {
+    state.launchTheme = payload;
+    localStorage.setItem("launchTheme", payload);
+  },
+  setLaunchIconTheme(state, payload) {
+    state.launchIconTheme = payload;
+    localStorage.setItem("launchIconTheme", payload);
   },
 };
