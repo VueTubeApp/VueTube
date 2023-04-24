e06649b8b8cfa8535d62f9f0d05b05e597728cf0
Alex <hello@missingprogram.me>
2023-04-24 19:58:51 +1200

    fix: 🐛 typo that caused build to fail

--- NUXT/pages/playlist.vue
+++ NUXT/pages/playlist.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script>
-import playlistVideoCard from "~/components/playlist/playlistVideoCard.vue";
+import playlistVideoCard from "~/components/Playlist/playlistVideoCard.vue";
 export default {
   components: { playlistVideoCard },
   computed: {
