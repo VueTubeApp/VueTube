938073477df717023f6c16248f8f4d9e61b0c6da
Georgiy <olenchenko.h.m@nmu.one>
2023-05-14 18:51:33 +0300

    fix(description): Fixed errors in description

--- NUXT/components/UtilRenderers/YtTextFormatterNew.vue
+++ NUXT/components/UtilRenderers/YtTextFormatterNew.vue
@@ -10,6 +10,7 @@
 export default {
   props: {
     textRuns: {},
+    additionalInfo: {},
   },
   emits: ["play", "pause"],
   data: () => ({
@@ -17,22 +18,21 @@ export default {
   }),
   computed: {
     formatTitle() {
-      let tempContent = "";
       let img =
         "<img src = 'https://www.gstatic.com/youtube/img/watch/yt_favicon.png' style=\"height: 10px; width: 14px;\"/>";
       if (this.textRuns.content && this.textRuns.commandRuns) {
-        tempContent = this.textRuns.content;
-        let arrayWithIndexes = [];
+        let tempContent = this.textRuns.content;
         let arrayWithReplaceParts = [];
         this.textRuns.commandRuns.forEach((commandRun) => {
-          arrayWithIndexes.push([commandRun.startIndex, commandRun.length]);
           //[textToReplace, urlFromEndpoint]
+          const textBeforeReplace = this.textRuns.content.substring(
+            commandRun.startIndex,
+            commandRun.startIndex + commandRun.length
+          );
+          // console.log("isExistInArray "+ textBeforeReplace + ": " + isExistInArray);
           if (commandRun.onTap.innertubeCommand.commandMetadata) {
             arrayWithReplaceParts.push([
-              this.textRuns.content.substring(
-                commandRun.startIndex,
-                commandRun.startIndex + commandRun.length
-              ),
+              textBeforeReplace,
               commandRun.onTap.innertubeCommand.commandMetadata
                 .webCommandMetadata.url,
             ]);
@@ -46,9 +46,31 @@ export default {
             ]);
           }
         });
-        console.log(arrayWithIndexes);
-        console.log(arrayWithReplaceParts);
 
+        const duplicates = [];
+        arrayWithReplaceParts = arrayWithReplaceParts.filter((subArr) => {
+          const isFirstElementDuplicate = duplicates.includes(subArr[0]);
+          if (isFirstElementDuplicate) {
+            return false;
+          } else {
+            duplicates.push(subArr[0]);
+            return true;
+          }
+        });
+        console.log(arrayWithReplaceParts);
+        for (let i = 0; i < arrayWithReplaceParts.length; i++) {
+          for (let j = i + 1; j < arrayWithReplaceParts.length; j++) {
+            if (
+              JSON.stringify(arrayWithReplaceParts[i]) ===
+              JSON.stringify(arrayWithReplaceParts[j])
+            ) {
+              arrayWithReplaceParts.splice(j, 1);
+              j--;
+            }
+          }
+        }
+
+        // Replacing urls in description
         arrayWithReplaceParts.forEach((text) => {
           if (text[1].indexOf("/hashtag/") > -1) {
             //skip
@@ -62,8 +84,11 @@ export default {
               img +
               nameOfUrl +
               "</a>";
-            tempContent = tempContent.replace(text[0], newUrl);
-          } else if (text[1].indexOf("/channel/") > -1) {
+            tempContent = tempContent.replaceAll(text[0], newUrl);
+          } else if (
+            text[1].indexOf("/channel/") > -1 ||
+            text[1].indexOf("youtube.com/c/") > -1
+          ) {
             let nameOfUrl = text[0].replace(/   /, " ");
             // let newUrl =
             //   "<a" + ' onclick=openInternal("' + text[1] + '") style="background-color: rgba(0,0,0,0.051); border-radius: 8px; white-space: nowrap;">' + img + nameOfUrl + "</a>";
@@ -75,7 +100,7 @@ export default {
               img +
               nameOfUrl +
               "</a>";
-            tempContent = tempContent.replace(text[0], newUrl);
+            tempContent = tempContent.replaceAll(text[0], newUrl);
           } else {
             let params = new Proxy(new URLSearchParams(text[1]), {
               get: (searchParams, prop) => searchParams.get(prop),
@@ -83,12 +108,13 @@ export default {
             let url = decodeURI(params.q);
             let newUrl =
               "<a" + ' onclick=openExternal("' + url + '")>' + text[0] + "</a>";
-            tempContent = tempContent.replace(text[0], newUrl);
+            tempContent = tempContent.replaceAll(text[0], newUrl);
           }
         });
+        return tempContent;
+      } else {
+        return this.textRuns.content;
       }
-      console.log(tempContent);
-      return tempContent;
     },
   },
   created() {
@@ -97,7 +123,6 @@ export default {
   },
   methods: {
     openExternal(url) {
-      console.log(url);
       this.$vuetube.openExternal(url);
     },
     async openInternal(url) {
