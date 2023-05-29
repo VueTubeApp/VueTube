c7c9a5bd68293c3bb8447bec501742b4f141877d
kylecz007 <134910715+kylecz007@users.noreply.github.com>
2023-05-29 09:13:02 +0200

    Create Slovak.js
    Not final, I'll talk to friend about it, but it should be 90% accurate

---
+++ NUXT/plugins/languages/Slovak.js
@@ -0,0 +1,93 @@
+module.exports = {
+  name: "Slovenština",
+
+  global: {
+    home: "Domov",
+    subscriptions: "Odbery",
+    library: "Knižnica",
+    restart: "Reštartovať",
+    later: "neskôr",
+    settingRestart: "Úpravy týchto nastavení vyžaduje reštart aplikácie pre prevedenie zmien."
+  },
+
+  index: {
+    connecting: "Pripojovanie",
+    plugins: "Načítanie doplnkov",
+    launching: "Spúšťanie",
+  },
+
+  settings: {
+    general: "Obecne",
+    theme: "Téma",
+    player: "Prehrávač",
+    uitweaker: "Rozhranie",
+    startupoptions: "Možnosti spustenie",
+    plugins: "Doplnky",
+    updates: "Aktualizácie",
+    logs: "Protokol",
+    about: "O aplikácii",
+    devmode: "Editor registrov",
+  },
+
+  mods: {
+    general: {
+      language: "Jazyk",
+    },
+    theme: {
+      normal: "Normálne",
+      adaptive: "Adaptívne",
+      custom: "Vlastné",
+      dark: "Tmavé",
+      black: "Čierne",
+      darkmode: "Tmavý režim",
+      darkmodetagline: "Bravo Six, Going Dark.",
+    },
+    tweaks: {
+      fullscreen: "Celá obrazovka",
+      navbarblur: "Rozmazať navigačnú lištu",
+      roundedcorners: "Zakulatené rohy",
+      roundthumbnails: "Zakulatené Náhledy",
+      roundwatchpagecomponents: "Zakulatené komponenty stránky sledovanie",
+      radius: "Polomer",
+    },
+    startup: {
+      defaultpage: "Výchozia stránka",
+    },
+    updates: {
+      install: "Inštalovať",
+      view: "Zobraziť",
+      latest: "Najnovšie",
+      installed: "Nainštalované",
+    },
+    logs: {
+      more: "Viac",
+    },
+    about: {
+      appinformation: "Informácie o aplikácii",
+      appversion: "Verzia aplikácie",
+      deviceinformation: "Informácie o zariadení",
+      platform: "Platforma",
+      os: "Operačný systém",
+      model: "Model",
+      manufacturer: "Výrobca",
+      emulator: "Emulátor",
+      github: "GitHub",
+      discord: "Discord",
+    },
+  },
+
+  events: {
+    welcome: "Vitajte vo VueTube",
+    tagline: "Budúcnosti streamovania videí",
+    next: "Ďalší",
+    updated: "VueTube bol aktualizován!",
+    awesome: "Úžasne!",
+    langsetup: "Poďme si vybrať jazyk!",
+    featuresetup: "Vyberťe si pár funkcií",
+    enableryd: "Povoliť Return YouTube Dislike",
+    enablespb: "Povoliť SponsorBlock",
+    thanks: "Ďakujeme za používanie VueTube",
+    enjoy: "Dúfame, že sa vám aplikácie bude ľúbiť",
+    packageinstaller: "Vyberte balíček ku sťaženie"
+  },
+};
